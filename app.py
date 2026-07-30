053c9b95a1a2922ca62dc890bea5ea913307bf19
Hafizin Zarif <hafizinzarif95@gmail.com>
2026-07-30 15:52:49 +0800

    Implement load and save functionality for expenses
    Added functions to load and save expenses from a JSON file.

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import streamlit as st
+import json
 
+def load_expenses():
+    try:
+        with open("expenses.json", "r") as file:
+            return json.load(file)
+
+    except FileNotFoundError:
+        return []
+
+def save_expenses(expenses):
+    with open("expenses.json", "w") as file:
+        json.dump(expenses, file, indent=4)
+        
 st.title("💰 Expense Tracker")
 
 if "expenses" not in st.session_state:
-    st.session_state.expenses = []
+    st.session_state.expenses = load_expenses()
 
 amount = st.number_input("Amount", min_value=0.0)
 
@@ -22,6 +35,8 @@ if st.button("Add Expense"):
     }
 
     st.session_state.expenses.append(expense)
+    
+    save_expenses(st.session_state.expenses)
 
     st.success("Expense added!")
 
