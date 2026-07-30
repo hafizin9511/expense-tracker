2504b93d3eb52a4107cb854cd85101fd6acbea21
Hafizin Zarif <hafizinzarif95@gmail.com>
2026-07-30 15:30:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.title("💰 Expense Tracker")
 
+expenses = []
+
 amount = st.number_input("Amount", min_value=0.0)
 
 category = st.text_input("Category")
@@ -11,4 +13,15 @@ description = st.text_input("Description")
 date = st.date_input("Date")
 
 if st.button("Add Expense"):
+    expense = {
+        "amount": amount,
+        "category": category,
+        "description": description,
+        "date": str(date)
+    }
+
+    expenses.append(expense)
+
     st.success("Expense added!")
+
+st.write(expenses)
